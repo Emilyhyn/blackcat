7ad7f3c8add3906d15fb5e63a6b25bafc1bdbf68
Emily <huangyannnig@sina.com>
2016-04-04 16:18:21 +1000

    remove laravel default file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 </div>
+                <div>  - Emily's Blackcat</div>
             </div>
         </div>
     </body>
