7ed18ca538e881668a1ad444f1b9571d30f834c8
Emily <huangyannnig@sina.com>
2016-04-07 16:54:09 +1000

    create navigation

---
+++ resources/views/templates/partials/alerts.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emily
+ * Date: 7/04/2016
+ * Time: 4:53 PM
+ */
---
+++ resources/views/templates/partials/navigation.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emily
+ * Date: 5/04/2016
+ * Time: 2:08 PM
+ */
