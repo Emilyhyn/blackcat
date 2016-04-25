4ce406e93ff328220fc9f6945dd64a902e00d9ef
Emily <huangyannnig@sina.com>
2016-04-25 15:15:08 +1000

    12/37(2)get search query

---
+++ resources/views/user/partials/userblock.blade.php
@@ -0,0 +1,8 @@
+<div class="media">
+    <a class="pull-left" href="#">
+        <img class="media-object" data-src="..." alt="">
+    </a>
+    <div class="media-body">
+        <h4 class="media-heading"><a href="#">Emily</a></h4>
+    </div>
+</div>
