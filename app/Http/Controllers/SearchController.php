306a0277b1f32b969da8dfb772c5610064985b45
Emily <huangyannnig@sina.com>
2016-04-25 15:49:26 +1000

    12/37(3)write mysql query to do actual search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,8 +8,23 @@
 
   namespace Blackcat\Http\Controllers;
 
+  use DB;
+  use Blackcat\Models\User;
+  use Illuminate\Http\Request;
+
+
   class SearchController extends Controller{
-      public function getResults(){
+      public function getResults(Request $request){
+          $query = $request->input('query');
+
+          if(!$query){
+              return redirect()->route('home');
+          }
+
+          $users = User::where(DB::raw("CONCAT(first_name, ' ', last_name)"),'LIKE', "%{$query}%")
+                   ->orWhere('username','LIKE',"%{$query}%")
+                   ->get();
+          dd($users);
           return view('search.results');
       }
   }
